4cbcf9eaa392340c9e03f2a864a47eb4ad93be9e
shaonlimit <shasanexp@gmail.com>
2023-01-27 16:40:03 +0600

    shaon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,18 +16,34 @@ const Sidebar = () => {
 	]
 
 	return (
-		<nav className='bg-green-500 h-screen w-60'>
-			<h2 className='text-xl font-bold'>Dashboard</h2>
-			<ul>
-				{sideBarOptions.map((item, index) => (
-					<li key={index}>
-						<Link to={item.url}>
-							<span>{item.name}</span>
-						</Link>
-					</li>
-				))}
-			</ul>
-		</nav>
+		<div className="flex">
+			<div className="flex flex-col h-screen bg-cyan-900	 shadow w-60">
+				<div className="space-y-3">
+					<div className="flex items-center text-center">
+						<img src="https://unsplash.com/photos/iEEBWgY_6lA" alt="" />
+						<h2 className="text-xl font-bold text-white ">User Name</h2>
+					</div>
+					<div className="flex-1">
+						<ul className="pt-2 pb-4 space-y-1 text-sm text-white font-medium">
+
+							{
+								sideBarOptions.map((bar, index) =>
+
+									<li className="rounded-sm hover:bg-cyan-700 transition duration-200 p-1" key={index}>
+										<Link to={bar.url} className="flex items-center p-2 space-x-3 rounded-md">
+											<span>{bar.name}</span>
+										</Link>
+									</li>
+								)
+							}
+
+
+						</ul>
+					</div>
+				</div>
+			</div>
+
+		</div >
 	)
 }
 

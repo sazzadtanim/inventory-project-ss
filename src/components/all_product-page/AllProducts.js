4e8579f384a4707ad43744fdd4ed3b6918a63319
shaonlimit <shasanexp@gmail.com>
2023-01-31 15:36:17 +0600

    modified

--- src/components/all_product-page/AllProducts.js
+++ src/components/all_product-page/AllProducts.js
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
+import AllProductTableHead from "./AllProductTableHead";
 import AllProductTableRow from "./AllProductTableRow";
+import SearchBox from "./SearchBox";
+import SearchButton from "./SearchButton";
+import SearchOptionBox from "./SearchOptionBox";
 
 const AllProducts = () => {
   const [products, setProducts] = useState([]);
@@ -12,20 +16,17 @@ const AllProducts = () => {
   return (
     <div className="flex flex-col">
       <div className="search-box">
-        <input type="text" className="w-50 border border-solid-black" />
-        <button className="bg-black text-white">SEARCH</button>
+        <SearchBox type="text" placeholder="Type to search" />
+
+        <select>
+          <SearchOptionBox />
+        </select>
+        <SearchButton />
       </div>
       <table className="border border-solid border-black">
         <thead className="w-screen">
           <tr className="bg-gray-400">
-            <th className="mt-2 mr-3 p-3">PRODUCT NAME</th>
-            <th className="mt-2 mr-3 p-3">SKU</th>
-            <th className="mt-2 mr-3 p-3">REGULAR PRICE</th>
-            <th className="mt-2 mr-3 p-3">SALE PRICE</th>
-            <th className="mt-2 mr-3 p-3">STOCK QUANTITY</th>
-            <th className="mt-2 mr-3 p-3">MANAGE STOCK</th>
-            <th className="mt-2 mr-3 p-3">EDIT</th>
-            <th className="mt-2 mr-3 p-3">VIEW PURCHASE</th>
+            <AllProductTableHead />
           </tr>
         </thead>
         <tbody>
